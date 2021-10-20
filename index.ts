339cc98e49bd773e67414379df5294f910fdd64d
015347 <qianzhixiang@htsc.com>
2021-10-20 13:15:38 +0800

    优化代码

--- index.ts
+++ index.ts
@@ -1,46 +1,31 @@
-import "reflect-metadata";
+import 'reflect-metadata';
 export type IConstructor<T> = new (...args: any[]) => T;
-const FUNCTIONS = Symbol("functions");
-const INSTANCES = Symbol("instances");
 
-var GLOBAL_HANDLER: Array<(v: any) => any> = [];
 export function Injectable<T>({
-  bootstrap = false,
+    bootstrap = false,
 }: { bootstrap?: boolean } = {}) {
-  return (target: IConstructor<T>) => {
-    const paramsTypes = Reflect.getMetadata("design:paramtypes", target);
-    Reflect.defineMetadata(FUNCTIONS, paramsTypes, target);
-    bootstrap && Ioc(target);
-  };
+    return (target: IConstructor<T>) => {
+        // const paramsTypes = Reflect.getMetadata('design:paramtypes', target);
+        bootstrap && Ioc(target);
+    };
 }
 
 // 获取某个依赖
 
 export function Ioc<T>(target: IConstructor<T>): T {
-  const instance = Reflect.getMetadata(INSTANCES, target);
-  if (instance) {
-    return instance;
-  }
-  // 获取这个target的依赖
-  const paramsTypes: any[] = Reflect.getMetadata(FUNCTIONS, target) || [];
-  // 接下来就需要获取所有的依赖
-  const params = paramsTypes.map((rely: any) => {
-    return Ioc(rely);
-  });
+    const instance = Reflect.getMetadata('qzx-ioc:instance', target);
+    if (instance) {
+        return instance;
+    }
+    // 获取这个target的依赖
+    const paramsTypes: any[] =
+        Reflect.getMetadata('design:paramtypes', target) || [];
+    // 接下来就需要获取所有的依赖
+    const params = paramsTypes.map((rely: any) => {
+        return Ioc(rely);
+    });
 
-  const tempInstance: T = new (target as any)(...params);
-  const result = GLOBAL_HANDLER.reduce((v, h) => h(v), tempInstance);
-  Reflect.defineMetadata(INSTANCES, result, target);
-  return result;
-}
-
-export function Iocable() {
-  return (target: any, key: string) => {
-    const type = Reflect.getMetadata("design:type", target, key);
-    target[key] = Ioc(type);
-  };
-}
-
-export function AddGlobalIocHandler(handler: (v: object) => object) {
-  GLOBAL_HANDLER.push(handler);
+    const result: T = new (target as any)(...params);
+    Reflect.defineMetadata('qzx-ioc:instance', result, target);
+    return result;
 }
